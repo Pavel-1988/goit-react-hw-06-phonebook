5afa4362bfc7c581df0f0a1df2519812123c3ebb
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-12-21 00:42:09 +0200

    update HApp

--- src/components/Happ2.jsx
+++ src/components/Happ2.jsx
@@ -1,14 +1,8 @@
-
-import { useSelector } from 'react-redux';
-import { getStatusContact } from 'redux/selectors';
-
 import { HContactForm } from './ContactForm/HContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
 export const App = () => {
-
-  const contacts = useSelector(getStatusContact)
 
     return (
       <>
